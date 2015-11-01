ffec675e0a81622131d98ecb81ff8f013a2a0769
junyakuroda <ku.jun29@gmail.com>
2015-11-01 08:13:38 +0000

    age

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20} , presence: true
     #　内容は必須かつ２文字以上３０文字以下
     validates :body , length: {minimum: 2 , maximum: 30} , presence: true
+    #年齢は０以上の数字のみ
+    validates :age , numericality: { only_integer: true, grater_than: 0 } , presence: true
 end
